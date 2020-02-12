f9685c7ba0c4d9e398c4344ce8c38da11e7905f0
wannasakon <wannasakon.ph.61@ubu.ac.th>
2020-02-12 20:09:06 +0700

    up

---
+++ webapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+from irisapp import views as irisview
+urlpatterns = [
+    path('', irisview.index),
+    path('houseapp/', irisview.index),
+    path('api/species', irisview.api_species),
+
+    path('matmul/', views.matmul),
+    path('admin/', admin.site.urls),
+]
